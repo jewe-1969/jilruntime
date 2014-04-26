fde001ca70c431bd88d5adf4fc276d645aebaa93
Stefan Kuhn <jewe-1969@users.noreply.github.com>
2014-04-26 22:59:30 +0000

    - added "log-garbage" setting "none" - added "document" option to language reference

--- jilruntime/include/jilversion.h
+++ jilruntime/include/jilversion.h
@@ -39,7 +39,7 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.40"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.41"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
