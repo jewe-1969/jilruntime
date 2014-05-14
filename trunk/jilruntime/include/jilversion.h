74f306b1d0b807d66c8a7e44a547dd9a1af6b647
s.kuhn2010@googlemail.com <s.kuhn2010@googlemail.com@119102e0-f051-4ccf-9035-df9876795754>
2014-05-14 18:23:11 +0000

    - fix [critical]: FixStackOffsetsInBranch() now properly traces all code paths of the function. A table is used for marking code paths already traced, so stack offsets are not fixed multiple times.

--- trunk/jilruntime/include/jilversion.h
+++ trunk/jilruntime/include/jilversion.h
@@ -39,7 +39,7 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.53"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.54"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
