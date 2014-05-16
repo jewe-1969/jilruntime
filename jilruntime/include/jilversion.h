a82923f3b4e4f0a7ecd88ac1929677577b8bbc4c
Stefan Kuhn <jewe-1969@users.noreply.github.com>
2014-05-16 09:24:11 +0000

    - fix: no error was returned when registering duplicate native type names

--- jilruntime/include/jilversion.h
+++ jilruntime/include/jilversion.h
@@ -39,7 +39,7 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.55"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.56"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
