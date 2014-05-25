bfa7f760e79e32fa6bc1814e87851d0dbb38aed7
Stefan Kuhn <jewe-1969@users.noreply.github.com>
2014-05-25 13:04:40 +0000

    - increased version info to reflect recent changes * NOT STABLE * use /tags/jewelscript_1_1_3_57_stable for now

--- jilruntime/include/jilversion.h
+++ jilruntime/include/jilversion.h
@@ -39,14 +39,14 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.57"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.60"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
 //------------------------------------------------------------------------------
 /// This is the version number of the JewelScript compiler.
 
-#define JIL_COMPILER_VERSION		JIL_PRODUCT_VERSION "3.33"
+#define JIL_COMPILER_VERSION		JIL_PRODUCT_VERSION "3.35"
 
 //------------------------------------------------------------------------------
 // JIL_MACHINE_VERSION
