a3aa3bbb772bcbac64d7ebd6b9694966aee478e3
Stefan Kuhn <jewe-1969@users.noreply.github.com>
2014-05-28 23:27:16 +0000

    - add: allow 'using' locally in class

--- jilruntime/include/jilversion.h
+++ jilruntime/include/jilversion.h
@@ -39,14 +39,14 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.68"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "3.69"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
 //------------------------------------------------------------------------------
 /// This is the version number of the JewelScript compiler.
 
-#define JIL_COMPILER_VERSION		JIL_PRODUCT_VERSION "3.43"
+#define JIL_COMPILER_VERSION		JIL_PRODUCT_VERSION "3.44"
 
 //------------------------------------------------------------------------------
 // JIL_MACHINE_VERSION
