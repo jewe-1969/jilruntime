c7878c52b67dbbf55592e5f93d69667853f8b911
jewe_1969 <jewe_1969@119102e0-f051-4ccf-9035-df9876795754>
2015-03-30 07:02:38 +0000

    - version UP!

--- trunk/jilruntime/include/jilversion.h
+++ trunk/jilruntime/include/jilversion.h
@@ -39,7 +39,7 @@
 /// version numbers below is increased due to a change, this version should be
 /// increased as well, in order to reflect this change.
 
-#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "4.17"
+#define JIL_LIBRARY_VERSION			JIL_PRODUCT_VERSION "4.18"
 
 //------------------------------------------------------------------------------
 // JIL_COMPILER_VERSION
@@ -53,7 +53,7 @@
 //------------------------------------------------------------------------------
 /// This is the version number of the virtual machine.
 
-#define JIL_MACHINE_VERSION			JIL_PRODUCT_VERSION "4.0"
+#define JIL_MACHINE_VERSION			JIL_PRODUCT_VERSION "4.1"
 
 //------------------------------------------------------------------------------
 // JIL_TYPE_INTERFACE_VERSION
